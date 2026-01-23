6f124b9ae6b3eb7fe1a8ee738ed48097f82a9cd4
zyad23e <zyade23@arizona.edu>
2026-01-23 14:59:15 -0700

    Initialized coach marketplace setup

---
+++ src/main/java/com/zyad/coachmarketplace/CoachMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.zyad.coachmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zyad/coachmarketplace/CoachMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyad.coachmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
